db02508087c0a1116faabddf941bb4030e69ca32
NARC0T1C <58453114+NARCOTIC@users.noreply.github.com>
2020-08-06 01:25:55 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        title: "When you use a library WiFi network, the library knows what sites you have visited",
+        choices: ["Simply True", "Simply False", "True, if I don't use a VPN", "False, Even if I use a VPN they could use monitoring programs to see what I'm doing"],
+        answer: "True, if I don't use a VPN"
+    },
+    {
+        title: "Is it okay to allow close friends to keep my personal documents or pictures?",
+        choices: ["Yes because they can be trusted", "No because they can't be trusted", "Yes if they store it in their computer", "No because even if they can be trusted they can lose my information to a hacker"],
+        answer: "No, They can be trusted but if they get hacked, My data gets compromised"
+    },
+    {
+        title: "During a web browsing session in private or incognito mode, third parties cannot monitor my behaviour",
+        choices: ["Always True", "Always False", "True, if I don't use a VPN", "False, even if I use a VPN"],
+        answer: "False, even if I use a VPN"
+    },
+    {
+        title: "It is safe to use the same password for multiple user accounts, as long as it contains a few different letters, numbers, or symbols.",
+        choices: ["Simply True", "False, When a Hacker knows one of your passwords, they can guess the others", "True, if I use a VPN", "True, if I use HTTPS"],
+        answer: "False, When a Hacker knows one of your passwords, they can guess the others"
+    },
+    {
+        title: "Is it okay to share the last 4 digits of my Social Security Number",
+        choices: ["True, If your employer needs it to sort documents", "True, as long as you don't share the full number", "False, last 4 digits are the most important and unique", "None of the Above"],
+        answer: "False, last 4 digits are the most important and unique"
+    },
+    {
+        title: "It is safe to upload my personal documents, photos to cloud services like Google Drive, iCloud, DropBox, etc.",
+        choices: ["True, They are well known companies", "True, They are secure and unhackable", "True, They can't see my stuff", "False, If somebody steel my password they can access them"],
+        answer: "False, If somebody steel my password they can access them"
+    }
+	,
+	{
+        title: "What is the Unhackable Operating System",
+        choices: ["MacOS/iOS", "Windows", "Linux", "None. Everything is Hackable"],
+        answer: "None. Everything is Hackable"
+    }
+];
